57e6750d7ce621fe3809733a37c983a5bdb44a8e
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-10-27 19:33:37 +0600

    implement context api for firebase authentication

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,22 @@ import Login from './Pages/Login';
 import Quiz from './Pages/Quiz';
 import Result from './Pages/Result';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AuthProvider from '../context/AuthContext';
 
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/signup" element={<Signup/>} />
-          <Route exact path="/login" element={<Login/>} />
-          <Route exact path="/quiz" element={<Quiz/>} />
-          <Route exact path="/result" element={<Result/>} />
-        </Routes>
-      </Layout>
+      <AuthProvider>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/quiz" element={<Quiz />} />
+            <Route exact path="/result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </AuthProvider>
     </Router>
   );
 }
