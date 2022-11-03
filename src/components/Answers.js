7797a99d9dacca5335aa59f8ee8e0faae60d7315
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-11-03 08:51:53 +0600

    Quiz page done, firebase data set functionalities implemented.

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import Checkbox from './Checkbox'
-import classes from "../Styles/Answers.module.css";
+import React from 'react';
+import Checkbox from './Checkbox';
+import classes from '../Styles/Answers.module.css';
 
-export default function Answers() {
+export default function Answers({ options = [], handleChange }) {
   return (
     <div className={classes.answers}>
-        <Checkbox className={classes.answer} text="mcq option"/>
+      {options.map((option, index) => (
+        <Checkbox
+          className={classes.answer}
+          text={option.title}
+          value={index}
+          checked={option.checked}
+          key={index}
+          onChange={(e) => handleChange(e, index)}
+        />
+      ))}
     </div>
-  )
+  );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,8 @@ function App() {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
             <Route exact path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-            <Route exact path="/quiz" element={<PrivateRoute><Quiz /></PrivateRoute>} />
-            <Route exact path="/result" element={<PrivateRoute><Result /></PrivateRoute>} />
+            <Route exact path="/quiz/:id" element={<PrivateRoute><Quiz /></PrivateRoute>} />
+            <Route exact path="/result/:id" element={<PrivateRoute><Result /></PrivateRoute>} />
           </Routes>
         </Layout>
       </AuthProvider>
