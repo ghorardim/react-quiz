ea076a1a5ca8de35e26e3cf1770ba7ec0c1b7ec5
abdullah.mahmud <abdullah.mahmud@bjitgroup.com>
2022-11-09 11:57:49 +0600

    implemented featch api, featch badge dynamically in result page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
 import classes from '../Styles/Summary.module.css';
-import image from '../assets/images/success.png';
+import successImage from '../assets/images/success.png';
+import useFetch from '../hooks/useFetch';
 
 export default function Summary({ score, noq }) {
+  const getkeyword = useMemo(() => {
+    if ((score / (noq * 5)) * 100 < 50) {
+      return 'failed';
+    } else if ((score / (noq * 5)) * 100 < 75) {
+      return 'good';
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return 'very good';
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return 'excellent';
+    }
+  }, [score, noq]);
+  const { loading, error, result } = useFetch(
+    `https://api.pexels.com/v1/search?query=${getkeyword}&per_page=1`,
+    'GET',
+    {
+      Authorization: process.env.REACT_APP_PEXELS_API_KEY,
+    }
+  );
+  const image = result ? result.photos[0].src.medium : successImage;
   return (
     <div className={classes.summary}>
       <div className={classes.point}>
@@ -13,9 +33,13 @@ export default function Summary({ score, noq }) {
         </p>
       </div>
 
-      <div className={classes.badge}>
-        <img src={image} alt="Success" />
-      </div>
+      {loading && <div className={classes.badge}>Loading your badge...</div>}
+      {error && <div className={classes.badge}>An error occured!</div>}
+      {!loading && !error && (
+        <div className={classes.badge}>
+          <img src={image} alt="Success" />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -17,7 +17,7 @@ export default function Videos() {
           next={() => setPage(page + 8)}>
           {videos.map((video) =>
             video.noq > 0 ? (
-              <Link to={`/quiz/${video.youtubeID}`} key={video.youtubeID}>
+              <Link to={`/quiz/${video.youtubeID}`} state={{videoTitle: video.title}} key={video.youtubeID}>
                 <Video
                   title={video.title}
                   id={video.youtubeID}
